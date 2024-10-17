65a4c50496659a2161475ade44cb6b8f2036c8c6
cns28 <cns28@njit.edu>
2024-10-17 18:39:32 -0400

    Completed REST API microservice

--- src/index.js
+++ src/index.js
@@ -13,6 +13,63 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+// POST
+let users = []
+let id_unq = 1
+app.post('/users', (req, res) => {
+    const {name, email} = req.body
+    if (!name || !email) {
+        return res.status(400).send({error: 'Name/email required'});
+    }
+    const new_user = {id: id_unq++, name, email};
+    users.push(new_user);
+    res.status(201).json(new_user);
+});
+
+// GET
+app.get('/users/:id', (req, res) => {
+    const user_id = parseInt(req.params.id)
+    const user = users.find(u => u.id === user_id);
+
+    if (!user) {
+        return res.status(404).send({ error: 'User not found' });
+    }
+    res.json(user);
+});
+
+// PUT
+app.put('/users/:id', (req, res) => {
+    const user_id = parseInt(req.params.id);
+    const { name, email } = req.body;
+    const user = users.find(u => u.id === user_id);
+
+    if (!user) {
+        return res.status(404).send({ error: 'User not found' });
+    }
+
+    if (!name || !email) {
+        return res.status(400).send({ error: 'Name/email required' });
+    }
+    user.name = name;
+    user.email = email;
+
+    res.json(user);
+});
+
+// DELETE
+app.delete('/users/:id', (req, res) => {
+    const user_id = parseInt(req.params.id);
+    const user_index = users.findIndex(u => u.id === user_id);
+
+    if (user_index === -1) {
+        return res.status(404).send({ error: 'User not found' });
+    }
+
+    users.splice(user_index, 1);
+
+    res.status(204).send();
+});
+
 // Do not touch the code below this comment
 // **************************************************************
 
